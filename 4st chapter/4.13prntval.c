b50901283b53480b5a9f6cce648198529b8fe35b
slowtalk11 <ympk@qq.com>
2020-07-13 02:37:51 +0800

    chapter4

---
+++ 4st chapter/4.13prntval.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) {
+	int bph2o = 212;
+	int rv;
+	rv = printf("%d F is water's boiling point.\n",bph2o);
+	printf("The printf() function printef %d characters.\n",rv);
+	return 0;
+}
+
+
+/*Ô¤¼Æ½á¹û£º
+0x31 0X31 0x31.**
+**42** 42**-42**
+**    6**  006**000006**  006** 
+
+*/ 
